83bd63dc779f31f8783f83c27ebbaa6ca76b4309
MHS99 <faresmhs99@gmail.com>
2019-12-19 15:17:19 +0200

    MHSAddedHaga

--- src/eg/edu/alexu/csd/oop/game/model/Logger/GameLogger.java
+++ src/eg/edu/alexu/csd/oop/game/model/Logger/GameLogger.java
@@ -5,27 +5,35 @@ import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 public class GameLogger {
-    public static void main(String[] args) {
+    private static GameLogger instance;
+    private FileHandler fileHandler;
+    public Logger logger;
+    public static GameLogger getInstance() {
 
-        Logger logger = Logger.getLogger("MyLog");
-        FileHandler fileHandler = null;
-            File file = new File("logger.txt");
-            try {
-                file.createNewFile();
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-            try {
-                fileHandler = new FileHandler("logger.txt", true);
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-            logger.addHandler(fileHandler);
-            SimpleFormatter formatter = new SimpleFormatter();
-            fileHandler.setFormatter(formatter);
-            logger.info("My first log");
-            logger.setUseParentHandlers(false);
-            logger.info("Hi How r u?");
+        if (GameLogger.instance == null) {
 
+            GameLogger.instance = new GameLogger();
         }
+
+        return GameLogger.instance;
     }
+
+    private GameLogger() {
+        File file = new File("logger.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        try {
+            fileHandler = new FileHandler("logger.txt", true);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        logger = Logger.getLogger("MyLog");
+        logger.addHandler(fileHandler);
+        SimpleFormatter formatter = new SimpleFormatter();
+        fileHandler.setFormatter(formatter);
+
+    }
+}
